3d4e39d7f90302f5f93200a57d45e8ea90520650
Junxian Qi <junxian.j.qi@gmail.com>
2020-10-24 13:42:23 +0800

    Assignment 1: React Components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {Card, CardBody, CardImg, CardText, CardTitle} from 'reactstrap';
+
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+          <div className="col-12 col-md-5 m-1">
+            <Card>
+              <CardImg top src={dish.image} alt={dish.name}/>
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+      );
+    } else {
+      return (
+          <div></div>
+      );
+    }
+  }
+
+  renderComments(selectedComments) {
+    const comments = selectedComments.map(selectedComment => {
+      return (
+          <li key={selectedComment.id}>
+            <p>{selectedComment.comment}</p>
+            <p>-- {selectedComment.author},
+              &nbsp;
+              {new Intl.DateTimeFormat('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: '2-digit',
+              }).format(new Date(selectedComment.date))}
+            </p>
+          </li>
+      );
+    });
+    return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">
+            {comments}
+          </ul>
+        </div>
+    );
+  }
+
+  render() {
+    const selectedDish = this.props.selectedDish;
+    let selectedDishDetail;
+    if (selectedDish != null) {
+      const dish = this.renderDish(selectedDish);
+      const comments = this.renderComments(selectedDish.comments);
+      selectedDishDetail = <div className="row">
+        {dish}
+        {comments}
+      </div>;
+    } else {
+      selectedDishDetail = <div></div>;
+    }
+    return (selectedDishDetail);
+  }
+}
+
+export default DishDetail;
